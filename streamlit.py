4fcabd4d1c390fb97d428f66eb44605c2abf2de7
Andréia Gonçalves <100314549+AndyGoncalves@users.noreply.github.com>
2022-11-14 12:38:28 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
 
 #bibliotecas com erro -----------------------
 #import plotly.graph_objects as go
