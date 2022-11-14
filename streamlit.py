9144956252db7079e40d1013ef8a0317d0fbbbe1
Andréia Gonçalves <100314549+AndyGoncalves@users.noreply.github.com>
2022-11-14 04:14:35 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,22 +7,9 @@ profissoes = pd.read_csv("profissoesti.csv")
 
 st.write(profissoes)
 
-nome_profissao = st.selectbox()
-
 name = st.text_input("Qual é o seu nome?")
 
 if(len(name)>0):
     st.write(f'Seu nome é {name}')
 else:
     st.write('Favor inserir seu nome!')
-
-
-data = pd.DataFrame({Prof:profissoes},index=[0])
-
-st.header("Suba um arquivo")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
